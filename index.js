4e38732602d0a28868f1bdbdf933eba59b25dcae
Marco Dall'Omo <marcodallomo@marcos-mbp.home>
2022-02-05 14:48:07 +0000

    added cors

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const dotenv = require("dotenv");
 const app = express();
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
+const cors = require("cors");
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 
 mongoose
